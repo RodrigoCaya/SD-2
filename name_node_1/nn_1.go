f382c3d7303b9602b0a8777e3090261bc16ca181
root <root@dist13.inf.santiago.usm.cl>
2020-11-18 19:43:55 -0300

    XD

---
+++ name_node_1/nn_1.go
@@ -0,0 +1,24 @@
+package main 
+import(
+	"log"
+	"net"
+	"google.golang.org/grpc"
+	"github.com/RodrigoCaya/SD-2/proto"
+)
+
+func conexioncl(){
+	liscliente, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+	s := proto.Server{}
+	grpcServer := grpc.NewServer()
+	proto.RegisterProtoServiceServer(grpcServer, &s)
+	if err := grpcServer.Serve(liscliente); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+}
+
+func main(){
+	conexioncl()
+}
