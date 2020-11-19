d69c39956f8a935a3d0e06c357b0261078bc0a6d
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-11-19 19:31:37 -0300

    probando dn y nn con cliente

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -4,11 +4,40 @@ import (
 	"log"	
 	"google.golang.org/grpc"
 	"context"
-	//"github.com/RodrigoCaya/SD-2/dn_proto"
+	"github.com/RodrigoCaya/SD-2/dn_proto"
 	"github.com/RodrigoCaya/SD-2/nn_proto"
 )
 
-func codigo(conn *grpc.ClientConn){
+func data_node(){
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("dist14:9001", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := dn_proto.NewHelloworldServiceClient(conn)
+		
+	message := dn_proto.CodeRequest{
+		Code: "hola",
+	}
+
+	response, err := c.Buscar(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling Buscar: %s", err)
+	}
+
+	log.Printf("%s", response.Code)
+}
+
+func name_node(){
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("dist13:9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
 	c := nn_proto.NewHelloworldServiceClient(conn)
 		
 	message := nn_proto.CodeRequest{
@@ -24,12 +53,27 @@ func codigo(conn *grpc.ClientConn){
 }
 
 func main() {
-	//nn
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial("dist13:9000", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("could not connect: %s", err)
+	
+	var first string
+
+	for{
+		fmt.Println("-----------------")
+		fmt.Println("Escoge: ") 
+		fmt.Println("(1) Subir libro") 
+		fmt.Println("(2) Descargar libro")
+		fmt.Println("(0) Salir")
+		fmt.Println("-----------------")
+		 	  
+		fmt.Scanln(&first)
+		if first == "1"{
+			go data_node()
+		}
+		if first == "2"{
+			go name_node()
+			
+		}
+		if first == "0"{
+			break
+		}
 	}
-	defer conn.Close()
-	codigo(conn)
 }
