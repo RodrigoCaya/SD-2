1ac547769702d073ca2e796ef91637eb66e8bc05
root <root@dist13.inf.santiago.usm.cl>
2020-11-22 20:10:42 -0300

    .proto fixed

--- dn_proto/dn.pb.go
+++ dn_proto/dn.pb.go
@@ -71,18 +71,68 @@ func (x *CodeRequest) GetCode() string {
 	return ""
 }
 
+type ChunkRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Chunk []byte `protobuf:"bytes,1,opt,name=chunk,proto3" json:"chunk,omitempty"`
+}
+
+func (x *ChunkRequest) Reset() {
+	*x = ChunkRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_dn_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ChunkRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ChunkRequest) ProtoMessage() {}
+
+func (x *ChunkRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_dn_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ChunkRequest.ProtoReflect.Descriptor instead.
+func (*ChunkRequest) Descriptor() ([]byte, []int) {
+	return file_dn_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *ChunkRequest) GetChunk() []byte {
+	if x != nil {
+		return x.Chunk
+	}
+	return nil
+}
+
 var File_dn_proto protoreflect.FileDescriptor
 
 var file_dn_proto_rawDesc = []byte{
 	0x0a, 0x08, 0x64, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x08, 0x64, 0x6e, 0x5f, 0x70,
 	0x72, 0x6f, 0x74, 0x6f, 0x22, 0x21, 0x0a, 0x0b, 0x43, 0x6f, 0x64, 0x65, 0x52, 0x65, 0x71, 0x75,
 	0x65, 0x73, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x32, 0x4d, 0x0a, 0x11, 0x48, 0x65, 0x6c, 0x6c, 0x6f,
-	0x77, 0x6f, 0x72, 0x6c, 0x64, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x38, 0x0a, 0x06,
-	0x42, 0x75, 0x73, 0x63, 0x61, 0x72, 0x12, 0x15, 0x2e, 0x64, 0x6e, 0x5f, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x2e, 0x43, 0x6f, 0x64, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x15, 0x2e,
-	0x64, 0x6e, 0x5f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x43, 0x6f, 0x64, 0x65, 0x52, 0x65, 0x71,
-	0x75, 0x65, 0x73, 0x74, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x09, 0x52, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x22, 0x24, 0x0a, 0x0c, 0x43, 0x68, 0x75, 0x6e, 0x6b,
+	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x14, 0x0a, 0x05, 0x63, 0x68, 0x75, 0x6e, 0x6b,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x05, 0x63, 0x68, 0x75, 0x6e, 0x6b, 0x32, 0x50, 0x0a,
+	0x0c, 0x43, 0x68, 0x75, 0x6e, 0x6b, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x40, 0x0a,
+	0x0d, 0x45, 0x6e, 0x76, 0x69, 0x61, 0x72, 0x5f, 0x63, 0x68, 0x75, 0x6e, 0x6b, 0x73, 0x12, 0x16,
+	0x2e, 0x64, 0x6e, 0x5f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x43, 0x68, 0x75, 0x6e, 0x6b, 0x52,
+	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x15, 0x2e, 0x64, 0x6e, 0x5f, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x2e, 0x43, 0x6f, 0x64, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x22, 0x00, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -97,13 +147,14 @@ func file_dn_proto_rawDescGZIP() []byte {
 	return file_dn_proto_rawDescData
 }
 
-var file_dn_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_dn_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_dn_proto_goTypes = []interface{}{
-	(*CodeRequest)(nil), // 0: dn_proto.CodeRequest
+	(*CodeRequest)(nil),  // 0: dn_proto.CodeRequest
+	(*ChunkRequest)(nil), // 1: dn_proto.ChunkRequest
 }
 var file_dn_proto_depIdxs = []int32{
-	0, // 0: dn_proto.HelloworldService.Buscar:input_type -> dn_proto.CodeRequest
-	0, // 1: dn_proto.HelloworldService.Buscar:output_type -> dn_proto.CodeRequest
+	1, // 0: dn_proto.ChunkService.Enviar_chunks:input_type -> dn_proto.ChunkRequest
+	0, // 1: dn_proto.ChunkService.Enviar_chunks:output_type -> dn_proto.CodeRequest
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -129,6 +180,18 @@ func file_dn_proto_init() {
 				return nil
 			}
 		}
+		file_dn_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ChunkRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -136,7 +199,7 @@ func file_dn_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_dn_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -158,72 +221,72 @@ var _ grpc.ClientConnInterface
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion6
 
-// HelloworldServiceClient is the client API for HelloworldService service.
+// ChunkServiceClient is the client API for ChunkService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type HelloworldServiceClient interface {
-	Buscar(ctx context.Context, in *CodeRequest, opts ...grpc.CallOption) (*CodeRequest, error)
+type ChunkServiceClient interface {
+	EnviarChunks(ctx context.Context, in *ChunkRequest, opts ...grpc.CallOption) (*CodeRequest, error)
 }
 
-type helloworldServiceClient struct {
+type chunkServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewHelloworldServiceClient(cc grpc.ClientConnInterface) HelloworldServiceClient {
-	return &helloworldServiceClient{cc}
+func NewChunkServiceClient(cc grpc.ClientConnInterface) ChunkServiceClient {
+	return &chunkServiceClient{cc}
 }
 
-func (c *helloworldServiceClient) Buscar(ctx context.Context, in *CodeRequest, opts ...grpc.CallOption) (*CodeRequest, error) {
+func (c *chunkServiceClient) EnviarChunks(ctx context.Context, in *ChunkRequest, opts ...grpc.CallOption) (*CodeRequest, error) {
 	out := new(CodeRequest)
-	err := c.cc.Invoke(ctx, "/dn_proto.HelloworldService/Buscar", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/dn_proto.ChunkService/Enviar_chunks", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// HelloworldServiceServer is the server API for HelloworldService service.
-type HelloworldServiceServer interface {
-	Buscar(context.Context, *CodeRequest) (*CodeRequest, error)
+// ChunkServiceServer is the server API for ChunkService service.
+type ChunkServiceServer interface {
+	EnviarChunks(context.Context, *ChunkRequest) (*CodeRequest, error)
 }
 
-// UnimplementedHelloworldServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedHelloworldServiceServer struct {
+// UnimplementedChunkServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedChunkServiceServer struct {
 }
 
-func (*UnimplementedHelloworldServiceServer) Buscar(context.Context, *CodeRequest) (*CodeRequest, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Buscar not implemented")
+func (*UnimplementedChunkServiceServer) EnviarChunks(context.Context, *ChunkRequest) (*CodeRequest, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method EnviarChunks not implemented")
 }
 
-func RegisterHelloworldServiceServer(s *grpc.Server, srv HelloworldServiceServer) {
-	s.RegisterService(&_HelloworldService_serviceDesc, srv)
+func RegisterChunkServiceServer(s *grpc.Server, srv ChunkServiceServer) {
+	s.RegisterService(&_ChunkService_serviceDesc, srv)
 }
 
-func _HelloworldService_Buscar_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CodeRequest)
+func _ChunkService_EnviarChunks_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ChunkRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(HelloworldServiceServer).Buscar(ctx, in)
+		return srv.(ChunkServiceServer).EnviarChunks(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/dn_proto.HelloworldService/Buscar",
+		FullMethod: "/dn_proto.ChunkService/EnviarChunks",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HelloworldServiceServer).Buscar(ctx, req.(*CodeRequest))
+		return srv.(ChunkServiceServer).EnviarChunks(ctx, req.(*ChunkRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _HelloworldService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "dn_proto.HelloworldService",
-	HandlerType: (*HelloworldServiceServer)(nil),
+var _ChunkService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "dn_proto.ChunkService",
+	HandlerType: (*ChunkServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Buscar",
-			Handler:    _HelloworldService_Buscar_Handler,
+			MethodName: "Enviar_chunks",
+			Handler:    _ChunkService_EnviarChunks_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
