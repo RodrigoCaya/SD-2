a5daea3b86188e90751cee31941bf8e7467b13de
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-11-24 17:43:04 -0300

    a ver si funciona dn1

--- data_node_1/dn_1.go
+++ data_node_1/dn_1.go
@@ -8,14 +8,86 @@ import(
 	"github.com/RodrigoCaya/SD-2/nn_proto"
 )
 
+type Server struct{
+	dn_proto.UnimplementedDnServiceServer
+}
+
+func distribuido(){
+	log.Printf("algoritmo distribuido")
+}
+
+func (s *Server) Propuesta(ctx context.Context, message *PropRequest) (*CodeRequest, error) {
+	return &CodeRequest{Code: "equis de"}, nil
+}
+
+func conectar(maquina string, prop string){
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(maquina, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := dn_proto.NewDnServiceClient(conn)
+		
+	message := dn_proto.PropRequest{
+		Propuesta: prop,
+	}
+
+	response, err := c.Propuesta(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling Buscar: %s", err)
+	}
+
+	log.Printf("%s", response.Code)
+}
+
+func centralizado(machine string){
+	var maquina string = ""
+	var prop string = ""
+	if machine == "4" {
+		maquina = "dist15:9002"
+		conectar(maquina, prop)
+		maquina = "dist16:9003"
+		conectar(maquina, prop)
+
+	}else{
+		if machine == "5" {
+			maquina = "dist14:9001"
+			conectar(maquina, prop)
+			maquina = "dist16:9003"
+			conectar(maquina, prop)
+
+		}else{
+			maquina = "dist14:9001"
+			conectar(maquina, prop)
+			maquina = "dist15:9002"
+			conectar(maquina, prop)
+
+		}
+	}
+	log.Printf("algoritmo centralizado")
+}
+
+func (s *Server) EnviarChunks(ctx context.Context, message *ChunkRequest) (*CodeRequest, error) {
+	//si es el ultimo chunk
+	if message.Tipo == "1"{
+		distribuido()
+	}else{
+		centralizado(message.Machine)
+	}
+	return &CodeRequest{Code: "chunk recibido"}, nil
+}
+
+
 func conexioncl(){
 	liscliente, err := net.Listen("tcp", ":9001")
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9001: %v", err)
 	}
-	s := dn_proto.Server{}
+	// s := dn_proto.Server{}
 	grpcServer := grpc.NewServer()
-	dn_proto.RegisterDnServiceServer(grpcServer, &s)
+	dn_proto.RegisterDnServiceServer(grpcServer, &Server{})
 	if err := grpcServer.Serve(liscliente); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9001: %v", err)
 	}
