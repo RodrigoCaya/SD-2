0a2b02d0270360a73c4d73fe67de98e92b4ab288
draakenz <43867892+draakenz@users.noreply.github.com>
2020-11-24 20:42:13 -0300

    funcion del caya y listas

--- nn_proto/funciones.go
+++ nn_proto/funciones.go
@@ -1,6 +1,10 @@
 package nn_proto
 
 import (
+	"os"
+	"fmt"
+	"log"
+	"bufio"
 	"golang.org/x/net/context"
 )
 
@@ -10,7 +14,7 @@ type Server struct{
 
 var nombres []string
 
-func (s *Server) Buscar(ctx context.Context, message *CodeRequest) (*CodeRequest, error) {
+func (s *Server) DisplayLista(ctx context.Context, message *CodeRequest) (*Lista, error) {
 	file, err := os.Open("log.txt")
 	if err != nil {
 		log.fatal(err)
@@ -24,5 +28,10 @@ func (s *Server) Buscar(ctx context.Context, message *CodeRequest) (*CodeRequest
 		nombres = append(nombres, scanner.Text())
 	}
 
-	return &CodeRequest{Code: nombres}, nil
+	return &Lista{L: nombres}, nil
+}
+
+func (s *Server) EnviarPropuesta(ctx context.Context, message *Propuesta) (*CodeRequest, error) {
+	log.Printf("Propuesta recibida")
+	return &CodeRequest{Code: "xd"}, nil
 }
