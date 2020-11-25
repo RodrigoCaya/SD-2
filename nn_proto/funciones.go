b2afa2bc334bc40112d661be5e403348bbde0a6e
draakenz <43867892+draakenz@users.noreply.github.com>
2020-11-25 03:01:00 -0300

    m

--- nn_proto/funciones.go
+++ nn_proto/funciones.go
@@ -16,7 +16,6 @@ type Server struct{
 
 
 var nombres string
-var ultimo int
 
 func (s *Server) DisplayLista(ctx context.Context, message *CodeRequest) (*CodeRequest, error) {
 	file, err := os.Open("log.txt")
