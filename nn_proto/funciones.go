da11747ae44964288cbb31cac50d282b9312cd44
draakenz <43867892+draakenz@users.noreply.github.com>
2020-11-25 03:04:27 -0300

    err?

--- nn_proto/funciones.go
+++ nn_proto/funciones.go
@@ -16,6 +16,7 @@ type Server struct{
 
 
 var nombres string
+var ultimo int
 
 func (s *Server) DisplayLista(ctx context.Context, message *CodeRequest) (*CodeRequest, error) {
 	file, err := os.Open("log.txt")
@@ -32,7 +33,7 @@ func (s *Server) DisplayLista(ctx context.Context, message *CodeRequest) (*CodeR
 		ultimostring := split[(len(split)) - 1]
 		fmt.Println("xd")
 		if ultimostring != ""{
-			ultimo, err := strconv.Atoi(ultimostring)
+			ultimo, err = strconv.Atoi(ultimostring)
 			if err != nil{
 				log.Fatal(err)
 			}
