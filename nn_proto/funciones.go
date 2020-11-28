590b187283a6af180c7694404ba3fa28fdc4b5c1
draakenz <43867892+draakenz@users.noreply.github.com>
2020-11-28 20:17:43 -0300

    create con frank

--- nn_proto/funciones.go
+++ nn_proto/funciones.go
@@ -70,7 +70,7 @@ func (s *Server) AgregarAlLog(ctx context.Context, message *Propuesta) (*CodeReq
 }
 	
 func agregarlog(c1 string, c2 string, c3 string, cantidadtotal string, nombrelibro string){
-	file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.Create("log.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
