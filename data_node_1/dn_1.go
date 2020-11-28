99c2c125f919dd4eb9d7ea894163d3fb7cda10bc
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-11-28 20:44:14 -0300

    mira jean

--- data_node_1/dn_1.go
+++ data_node_1/dn_1.go
@@ -113,6 +113,14 @@ func distribuido(cantidad int, nombrelibro string){
 		}
 		log.Printf("algoritmo distribuido")
 	}
+	if message.Cantidadn2 != "0"{
+		maquina = "dist15:9002"
+		conectardn(maquina, message)
+	}
+	if message.Cantidadn3 != "0"{
+		maquina = "dist16:9003"
+		conectardn(maquina, message)
+	}
 }
 
 func propuestadn(maquina string, message dn_proto.PropRequest) string {
